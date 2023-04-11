b4ef277a7bb6885958e214caf290966231937733
IshchenkoDan <7+;V7eB-42jVHK>
2023-04-11 15:58:31 +0300

    Create project commit

---
+++ src/main/java/com/example/QuestMisto/QuestMistoApplication.java
@@ -0,0 +1,13 @@
+package com.example.QuestMisto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestMistoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestMistoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/QuestMisto/QuestMistoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.QuestMisto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestMistoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
