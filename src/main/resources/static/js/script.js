cf7c9501a456bc7658f9fe009a0d2546901443c5
LEGION <v.babiy75@gmail.com>
2023-05-24 10:43:39 +0300

    MST-73 make login and signin and write tests

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,5 +1,5 @@
-var burgerMenu = document.getElementById("burger-menu");
-var burgerBtn = document.getElementById("burger-btn");
+var burgerMenu = document.querySelector(".burger-menu");
+var burgerBtn = document.querySelector(".burger-menu-btn");
 
 burgerBtn.addEventListener("click", function() {
   burgerMenu.classList.toggle("open");
@@ -12,3 +12,49 @@ document.addEventListener("click", function(event) {
     burgerMenu.classList.remove("open");
   }
 });
+
+
+
+
+window.addEventListener('wheel', function(event) {
+  if (event.deltaY > 0) {
+    scrollToNextSection();
+  } else if (event.deltaY < 0) {
+    scrollToPreviousSection();
+  }
+});
+
+function scrollToNextSection() {
+  var currentSection = getCurrentSection();
+  var nextSection = currentSection.nextElementSibling;
+
+  if (nextSection !== null) {
+    nextSection.scrollIntoView();
+    window.scrollBy(0, -100);
+  }
+}
+
+function scrollToPreviousSection() {
+  var currentSection = getCurrentSection();
+  var previousSection = currentSection.previousElementSibling;
+
+  if (previousSection !== null) {
+    previousSection.scrollIntoView();
+    window.scrollBy(0, 100);
+  }
+}
+
+function getCurrentSection() {
+  var sections = document.querySelectorAll('.quest');
+  var currentSection = null;
+
+  sections.forEach(function(section) {
+    var rect = section.getBoundingClientRect();
+
+    if (rect.top <= window.innerHeight / 2 && rect.bottom >= window.innerHeight / 2) {
+      currentSection = section;
+    }
+  });
+
+  return currentSection;
+}
